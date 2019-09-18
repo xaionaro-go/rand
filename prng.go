7252270bc7e6dae68f8b02a0cd902feec79c72eb
Dmitry Yu Okunev <xaionaro@dx.center>
2019-09-18 21:38:12 +0300

    Added few words about structure `PRNG`

--- prng.go
+++ prng.go
@@ -8,6 +8,7 @@ var (
 	globalPRNG = New()
 )
 
+// PRNG is a instance of a pseudo-random number generator.
 type PRNG struct {
 	uint64nPosition     uint64
 	uint64nPositionSafe uint64
@@ -15,10 +16,15 @@ type PRNG struct {
 	uint32nPositionSafe uint32
 }
 
+// New creates new instance of pseudo-random number generator. It
+// may be useful to generate separate instances for concurrent goroutines
+// to avoid using of `Safe()` functions.
 func New() *PRNG {
 	return NewWithSeed(uint64(time.Now().UnixNano()))
 }
 
+// NewWithSeed is the same as `New` but initializes the PRNG with predefined
+// seed.
 func NewWithSeed(seed uint64) *PRNG {
 	return &PRNG{
 		seed,
